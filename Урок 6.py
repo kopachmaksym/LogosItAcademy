c0088c88ae06582a2f8e3e8cfc4b337acf7b63fb
kopachmaksym <93545242+kopachmaksym@users.noreply.github.com>
2022-05-15 21:18:22 +0300

    Create Урок 6.py

---
+++ Урок 6.py
@@ -0,0 +1,30 @@
+lst = []
+n = int(input('Введіть кількість елементів(для 1 і 2 завдання): '))
+for i in range(n):
+    var = int(input('1)Цифра\n2)Стрічка\n\tВведіть цифру: '))
+    if var == 1:
+        lst.append(int(input('Введіть цифру: ')))
+    elif var == 2:
+        lst.append(input('Введіть цифру: '))
+
+print(list(map(lambda i: str(i) if type(i) == int else int(i) ,lst))) #1
+
+def func(i):    #2
+    if type(i) == str:
+        i = int(i)
+    else:
+        i = str(i)
+    return i
+lst_new = list(map(func,lst))
+print(lst_new)
+
+def func_2(*args): #3
+    def maps(i):
+        if type(i) == str:
+            i = int(i)
+        return i
+    st = list(map(maps,args))
+    st = set(st[0:9])
+    print(st)
+
+func_2(1,2,3,4,5,6,8,7,9,'10',11)
